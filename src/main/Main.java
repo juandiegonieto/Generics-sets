c285ee9322ce23fd8149316ad613b4db4f9d29eb
Jhonatan Castaño <jhonatancastanoibarra@gmail.com>
2022-03-05 08:44:21 -0500

    Finish version

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,101 @@
 package main;
 
+import model.Conjunto;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Set;
 
-public class Main <T>{
-
-	public static void main(String[] args) {
+public class Main <E>{
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+	public static <E> void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<T>array = new ArrayList<>();
-		Conjunto c = new Conjunto();
+		
+		try {
+			start();
+		} catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
-
+	
+	public static <E> void start ( ) throws NumberFormatException, IOException {
+		ArrayList<E>array = new ArrayList<>();
+		System.out.println("Write the size of the set");
+		int size = Integer.parseInt(br.readLine());
+		System.out.print("Write the elemtns of the set");
+		int i = 0;
+		while(size>i) {
+		array.add((E) br.readLine());
+		i++;}
+		Conjunto<E> c = new Conjunto<E>(array);
+		System.out.println("Elige una de las siguientes opciones:");
+		int operation = showMenu();
+		Set<E> a = askSet();
+		calculate(operation,c,a);
+	}
+	
+	public static int showMenu() throws NumberFormatException, IOException {
+		System.out.println("1)Union."+"\n"+"2)Interseccion"+"\n"+"3)Diferencia simetrica");
+		int op = 0;
+		try {
+			op = Integer.parseInt(br.readLine());
+			while(op>3 || op<1) {
+				System.out.print("No es una opcion valida, pruba de nuevo");
+				op =Integer.parseInt(br.readLine());
+			}
+		}catch(InputMismatchException ex) {
+			System.out.println("No has digitado numero, pruba de nuevo");
+			showMenu();
+			
+		}
+		return op;
+	}
+	
+	public static<E> void calculate(int op,Conjunto<E> c,Set<E>set1) {
+		
+		
+		
+		switch (op) {
+		case 1:
+			c.union(set1);
+			break;
+		case 2:
+			c.interseccion(set1);
+			break;
+		case 3:
+			c.diferencia(set1);
+			break;
+		}
+		
+	}
+	
+	public static<E> Set<E> askSet() throws NumberFormatException, IOException {
+		int quantity = 0;
+		try {
+			System.out.println("Digita la cantidad de objetos que tendra el conjunto con el que se realizara la operacion");
+			quantity = Integer.parseInt(br.readLine());
+			while(quantity<0) {
+				System.out.print("No es una opcion valida, pruba de nuevo");
+				quantity = Integer.parseInt(br.readLine());
+			}
+		}catch(InputMismatchException ex) {
+				System.out.println("No has digitado numero, pruba de nuevo");
+				askSet();
+		}
+		String element;
+		Set<E> s = new HashSet<E>(); 
+		
+		for (int i = 0; i<quantity;i++) {
+			System.out.println("Digita el "+(i+1)+" elemento del conjunto");
+			element = br.readLine();
+			s.add((E) element);
+		}
+		return s;
+		
+	}
 }
--- src/model/Conjunto.java
+++ src/model/Conjunto.java
@@ -1,13 +1,44 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-class Conjunto <E>{
-	public Conjunto(E[] set) {
-		Set<E> a = new HashSet<E>();
-		for(E s : set)
+public class Conjunto <E>{
+	Set<E> a;
+	public Conjunto(ArrayList<E> array) {
+		a = new HashSet<E>();
+		for(E s : array)
 		a.add(s);
 	}
 	
+	public void union(Set<E> set1) {
+		Set<E> union = new HashSet<E >(a); 
+		union.addAll(set1);
+		System.out.print("La union de ambos conjutos es la siguiente ");
+		System.out.println(union+"\n");
+	}
+	
+	public void interseccion(Set<E> set1) {
+		Set<E> interseccion = new HashSet<E >(a); 
+		set1.removeAll(set1);
+		interseccion.removeAll(set1);
+		if (interseccion.size()!=0) {
+			System.out.print("La interseccion de ambos conjutos es la siguiente ");
+			System.out.println(interseccion+"\n");
+		}else{
+			System.out.println("la interseccion es vacio");
+		}
+		
+	}
+	
+	public void diferencia(Set<E> set1) {
+		set1.removeAll(a);
+		if (set1.size()!=0) {
+			System.out.print("La diferencia c1-c2 es la siguiente ");
+			System.out.println(set1+"\n");
+		}else{
+			System.out.println("la resta es vacia");
+		}
+	}
 }
